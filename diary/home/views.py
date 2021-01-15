4a96e947e34e854551c64fbde536148a59aff732
Aabid Majid <aabidmajid26@gmail.com>
2021-01-15 18:50:03 +0530

    Successfully added Sessions feature

--- diary/home/views.py
+++ diary/home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render,HttpResponse
 from django.urls import reverse
 from django.contrib.admin import widgets
 import datetime
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 # people = dict()   #people
@@ -16,19 +17,22 @@ class NewEntryForm(forms.Form):
     name = forms.CharField(label = "Name",max_length=40)
     debit = forms.IntegerField(label = "Taken", initial=0,min_value=0)
     credit = forms.IntegerField(label="Given",initial=0,min_value=0)
-    date = forms.DateField(
-        label="On ", 
-        initial=datetime.date.today,
-        widget=forms.widgets.DateInput(attrs={'type':'date'}))
+    # date = forms.DateField(
+    #     label="On ", 
+    #     initial=datetime.date.today,
+    #     widget=forms.widgets.DateInput(attrs={'type':'date'}))
 
 
 
 def home(request):
-    request.session.modified = True
+    print("people" in request.session)
     if "people" not in request.session:
-        request.session['people'] = dict({'hello':1})
+        request.session['people'] = []
+        print('Initialized request.session["people"] to a list')
     if "accounts" not in request.session:
-        request.session['accounts'] = dict({'hello':[[12,12,2020-20-20]]})
+        request.session['accounts'] = dict()
+        print('Initialized request.session["acount"] to a dictionary')
+
     return render(request,"home/home.html")
 
 def showAll(request):
@@ -40,46 +44,20 @@ def add(request):
         form = NewEntryForm(request.POST)
         if form.is_valid():
             person = form.cleaned_data['name']
-            s = [form.cleaned_data['debit'],form.cleaned_data['credit'],form.cleaned_data['date']]
-            request.session['people'][person]=1
+            s = [form.cleaned_data['debit'],form.cleaned_data['credit']]
+            if person not in request.session['people']:
+                request.session['people'] += [person]
             try:
-                request.session['accounts'][person]+=[s]
+                request.session['accounts'][person].append(s)
                 print('appended to accounts[',person,']')
             except Exception:
                 request.session['accounts'][person] = [s]
                 print('added to accounts[',person,']')
+            return HttpResponseRedirect(redirect_to=reverse('people'))
         else:
             return render(request,"home/add.html",{"form":form})
     return render(request, "home/add.html",{"form":NewEntryForm()})
 
-
-
-
-
-# def add(request):
-#     if request.method == 'POST':
-#         form = NewEntryForm(request.POST)
-#         if form.is_valid():
-#             person = form.cleaned_data['name']
-#             s = [form.cleaned_data['debit'],form.cleaned_data['credit'],form.cleaned_data['date']]
-#             people[person]=1
-#             try:
-#                 accounts[person].append(s)
-#                 print(s,'appended to', person)
-#                 print(accounts)
-#             except Exception:
-#                 accounts[person] = [s]
-#                 print(s,'added to ', person)
-#                 print(accounts)
-#         else:
-#             return render(request,"home/add.html", {
-#                 "form":form
-#             })
-
-#     return render(request, "home/add.html",
-#     {
-#         "form":NewEntryForm()
-#     })
 
 def showPerson(request,name):
     print(request.session['accounts'])
